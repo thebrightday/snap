d89a7417ecaf837b46ca0b9a2ad34c8c0fdfc4ed
배병재 <39183208+BaeByungjae@users.noreply.github.com>
2022-08-19 23:37:58 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
->>>>>>> f804d55dc62965dccd1702a2314c6c3015389cb8
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -19,10 +12,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-<<<<<<< HEAD
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-=======
->>>>>>> f804d55dc62965dccd1702a2314c6c3015389cb8
